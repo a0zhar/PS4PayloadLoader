c245ed7cd9d18ede2fd7f30a3e2e31f0bc2b0381
a0zhar <96749372+a0zhar@users.noreply.github.com>
2023-06-09 17:00:30 +0200

    Commented struct members

--- jbcLib/jailbreak.h
+++ jbcLib/jailbreak.h
@@ -1,21 +1,23 @@
 #pragma once
 #include "defs.h"
 
-struct jbc_cred {
-    uid_t uid;
-    uid_t ruid;
-    uid_t svuid;
-    gid_t rgid;
-    gid_t svgid;
-    uintptr_t prison;
-    uintptr_t cdir;
-    uintptr_t rdir;
-    uintptr_t jdir;
-    uint64_t sceProcType;
-    uint64_t sonyCred;
-    uint64_t sceProcCap;
-};
+typedef unsigned int uid_t;
+typedef unsigned int gid_t;
 
+struct jbc_cred {
+    uid_t uid;            // effective user ID (alias euid)
+    uid_t ruid;           // real user ID
+    uid_t svuid;          // saved UID from a setuid executable
+    gid_t rgid;           // real group ID
+    gid_t svgid;          // saved gid from a setgid executable
+    uintptr_t prison;     // i think prison structure associated with the process
+    uintptr_t cdir;       // current directory
+    uintptr_t rdir;       // root directory
+    uintptr_t jdir;       // jail directory
+    uint64_t sceProcType; // value indicating the type of SCE process.
+    uint64_t sonyCred;    // credential associated with Sony.
+    uint64_t sceProcCap;  // the capability of the SCE process.
+};
 
 uintptr_t jbc_get_prison0(void);
 uintptr_t jbc_get_rootvnode(void);
